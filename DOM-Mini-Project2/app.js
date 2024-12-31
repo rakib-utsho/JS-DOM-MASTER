8b3af33d5068b567e1cd2f66faf005326b1e6274
rakib-utsho <rakibutsho1920@gmail.com>
2025-01-01 04:37:28 +0600

    thirde project done

--- DOM-Mini-Project2/app.js
+++ DOM-Mini-Project2/app.js
@@ -2,6 +2,7 @@
  * Project Requirements:
  * - Change the background color by generating random hex color by clicking a button
  * - Also display the hex code toa disabled input field
+ *
  */
 
 // step 1 - create onload handler
--- DOM-Mini-Project3/app.js
+++ DOM-Mini-Project3/app.js
@@ -2,9 +2,42 @@
  * Project Requirements:
  * - Change the background color by generating random hex color by clicking a button
  * - Also display the hex code toa disabled input field
+ * - copy the hex code from the input field by the copy button
  */
 
 // step 1 - create onload handler
 window.onload = () =>{
     main();
+}
+
+function main(){
+
+    // step 3 - collect all necessary references
+    const root = document.getElementById('root');
+    const btn = document.getElementById('change-btn');
+    const output = document.getElementById('output');
+    const copyBTN = document.getElementById('copy');
+
+        // step 4 - handel the change color click event
+    btn.addEventListener('click', function () {
+        const bgColor = generateHEXColor();
+        root.style.backgroundColor = bgColor;
+        output.value = bgColor;
+    });
+
+    // step 5 - handel the copy click event
+    copyBTN.addEventListener('click', function () {
+        navigator.clipboard.writeText(output.value);
+    })
+}
+
+// step 2 - random color generator function
+function generateHEXColor(){
+    // hex = #00000  #ffffff
+    // 255, 255, 255 --> #FFFFFF
+    const red = Math.floor(Math.random() * 255);
+    const green = Math.floor(Math.random() * 255);
+    const blue = Math.floor(Math.random() * 255);
+
+    return `#${red.toString(16)}${green.toString(16)}${blue.toString(16)}`;
 }
