6d5a173a9e993b17ae6e38992108b64b807a8195
rakib-utsho <rakibutsho1920@gmail.com>
2025-01-01 04:27:40 +0600

    thirde project js function

---
+++ DOM-Mini-Project3/app.js
@@ -0,0 +1,10 @@
+/**
+ * Project Requirements:
+ * - Change the background color by generating random hex color by clicking a button
+ * - Also display the hex code toa disabled input field
+ */
+
+// step 1 - create onload handler
+window.onload = () =>{
+    main();
+}
